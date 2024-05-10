38c6e8be185ea39464c211d533d708edb12fc02e
gabrielatwell1987 <gabrielatwell@proton.me>
2024-05-10 13:56:43 -0700

    updated landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,14 +10,15 @@
 		const img = document.querySelector('.img');
 
 		gsap.set([title, content, img], { opacity: 1 });
-		gsap.set(img, { scale: 1.25, x: 150, y: -100 });
+		gsap.set(img, { x: 0, y: -120 });
 
 		const tl = gsap.timeline({ defaults: { ease: 'expo' } });
-		tl.from(title, { duration: 2.5, x: -100, opacity: 0 })
+		tl.from(title, { duration: 2.5, x: 100, opacity: 0 })
 			.from(content, { duration: 2, x: 100, opacity: 0 })
 			.from(img, {
 				duration: 3,
-				scale: 1,
+				scale: 1.25,
+				x: 0,
 				y: 0,
 				opacity: 0,
 				transformOrigin: 'center center'
@@ -51,11 +52,12 @@
 
 <style>
 	p {
-		margin: 3% 0 3% 50%;
+		margin: 3% 0 3% 45%;
 		letter-spacing: 5px;
 		line-height: 1.75;
 		font-size: 1.1rem;
 		font-weight: 300;
+		width: 50%;
 	}
 
 	.content {
@@ -64,12 +66,13 @@
 
 	.image {
 		position: relative;
+		width: 75%;
 	}
 
 	.img {
 		height: auto;
 		position: absolute;
-		top: -240px;
+		top: -180px;
 		left: 250px;
 		border-radius: 0.25rem;
 	}
